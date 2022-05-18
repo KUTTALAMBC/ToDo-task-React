87b0d5213c8001555214e569427de9bc8d3f5060
kuttalam <kuttalambc@gmail.com>
2022-05-18 13:43:39 +0530

    FINAL COMMIT tictactoe in react

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React, {useState} from 'react';
 import './App.css';
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
+import TicTacToe from './TicTacToe';
 
 function App() {
+  return(
+    <TicTacToe></TicTacToe>
+  )
 
-  const [input, setInput] = useState('');
-  const [todos, setTodos] = useState([]);
-
-
-
-  return (
-    <div className="container">
-     <div className='app-wrapper'>
-     <div className="header">
-        <h1>ToDo-LIST</h1>
-    </div>
-      <div>
-        <TodoForm
-        input = {input}
-        setInput = {setInput}
-        todos = {todos}
-        setTodos = {setTodos}
-        ></TodoForm>
-      </div>
-      <div>
-        <TodoList todos={todos} setTodos={setTodos}></TodoList>
-      </div>
-     </div>
-    </div>
-  );
+  
 }
 
 export default App;
