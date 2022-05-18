dcfd91791e3ab77396e93f89d1fda37fd20f3ba6
kuttalam <kuttalambc@gmail.com>
2022-05-18 14:00:07 +0530

    final commit todolist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, {useState} from 'react';
 import './App.css';
-import TicTacToe from './TicTacToe';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 function App() {
-  return(
-    <TicTacToe></TicTacToe>
-  )
 
-  
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+
+
+  return (
+    <div className="container">
+     <div className='app-wrapper'>
+     <div className="header">
+        <h1>Todo-List</h1>
+    </div>
+      <div>
+        <TodoForm
+        input = {input}
+        setInput = {setInput}
+        todos = {todos}
+        setTodos = {setTodos}
+        ></TodoForm>
+      </div>
+      <div>
+        <TodoList todos={todos} setTodos={setTodos}></TodoList>
+      </div>
+     </div>
+    </div>
+  );
 }
 
 export default App;
